38c20325c6f0cdc4fbdac3ba5dd5adef893a9f00
lost731 <lost731@naver.com>
2022-01-03 16:53:30 +0900

    Add: SignUp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+import Link from 'next/link';
 function Home () {
-  return<div>Hello next!</div>
+  return<div><Link href="/SignUp">회원가입</Link></div>
 }
 
 export default Home;
