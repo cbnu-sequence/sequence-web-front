4a7acf5164415ab8f6ccd36207d6425c92fd8ba9
olo90632951 <olo90632951@gmail.com>
2022-03-27 11:29:20 +0900

    로그인, 회원가입 렌더링 테스트코드 (#3)

---
+++ pages/LogIn/test/Login.test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react';
+import { renderWithQueryClient } from '../../../test-utils';
+import Index from '../index';
+
+test('UItest', () => {
+  renderWithQueryClient(<Index />);
+
+  const LabelTest = screen.getAllByLabelText(/이메일 주소|비밀번호/);
+  //console.log(LabelTest);
+  expect(LabelTest).toHaveLength(2);
+
+  const LinkTest = screen.getAllByRole('link');
+  //console.log(LinkTest);
+  expect(LinkTest).toHaveLength(2);
+});
