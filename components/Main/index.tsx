603da3f358a7f329cc8db39a795a0c168f5aad9c
songhaeunsong <gkdms3469@naver.com>
2022-04-13 17:36:55 +0900

    add: 뽀모도로(홈화면)

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { MainDiv, Github } from './styles';
+import { MainDiv, Pomodoro, PomoLink, Github } from './styles';
+import Link from 'next/link';
 
 function Main() {
   return (
@@ -19,13 +20,28 @@ function Main() {
         </div>
       </div>
       <div className="main__page2"></div>
-      <div className="main__page3"></div>
+      <Pomodoro>
+        <div className="pomodoro--text">
+          <img src="/title_pomodoro.png" />
+          <p>
+            Why Pomodoro?
+            <br />
+            뽀모도로 기법은 시간 관리 방법론으로, 타이머를 이용해서 25분간 <br />
+            집중해서 일을 한 다음 5분간 휴식하는  방식입니다. 시퀀스에서는 <br />
+            개발자로서 '집중과 효율의 극대화'를 위해 뽀모도로를 선택했습니다.
+          </p>
+        </div>
+        <PomoLink href="/ppomodoro">
+          <img src="/tomato.png" className="tomato_btn" />
+        </PomoLink>
+      </Pomodoro>
       <div className="main__page4"></div>
       <Github>
-        <img src="/title_github.png" />
         <h2>Sequence</h2>
         <h3>github</h3>
-        <img src="/github.png" />
+        <Link href="https://github.com/cbnu-sequence">
+          <img src="/github.png" />
+        </Link>
       </Github>
     </MainDiv>
   );
--- components/Main/styles.tsx
+++ components/Main/styles.tsx
@@ -1,18 +1,19 @@
 import { sqBlack, sqRed, sqWhite } from '../../styles/constants';
 import styled from '@emotion/styled';
 import { keyframes } from '@emotion/react';
+import Link from 'next/link';
 
 const textAppear = keyframes`
   0% {
     width: 0px;
     opacity:0;
   }
-  10% {
+  15% {
     width: 450px;
     opacity:0;
 
   }
-  20% {
+  25% {
     width: 450px;
     opacity:1;
   }
@@ -40,7 +41,7 @@ export const MainDiv = styled.div`
     }
   }
   .main__top--text {
-    animation: ${textAppear} 10s ease-in-out infinite;
+    animation: ${textAppear} 7s ease-in-out infinite;
 
     margin-top: 40px;
 
@@ -57,16 +58,67 @@ export const MainDiv = styled.div`
   }
 `;
 
+export const Pomodoro = styled.div`
+  margin: 300px 50px 0 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .tomato_btn {
+    cursor: pointer;
+    width: 300px;
+    transition: width 0.2s ease-in-out;
+
+    &: hover {
+      width: 305px;
+    }
+  }
+
+  & > .pomodoro--text {
+    width: 470px;
+    & > img {
+      width: 400px;
+      margin-left: -10px;
+    }
+    & > p {
+      color: #107c10;
+      font-size: 16px;
+      margin-top: 80px;
+    }
+  }
+`;
+export const PomoLink = styled(Link)`
+  margin-left: 50px;
+  background-color: pink;
+  height: 300px;
+  width: 300px;
+`;
+
 export const Github = styled.div`
+  margin-top: 300px;
+  margin-bottom: 300px;
   display: flex;
   flex-direction: column;
   align-items: center;
   & > img {
+    cursor: pointer;
+    transition: height 0.2s ease-in-out;
+    margin-top: 50px;
+    margin-bottom: 80px;
     height: 150px;
+    &:hover {
+      height: 155px;
+    }
   }
-  & > h1 {
-    font-size: 100px;
-    font-family: 'Noto Sans KR', sans-serif;
+  & > h2 {
+    font-size: 70px;
+    font-weight: 900;
+    font-family: 'Noto Serif KR', serif;
+  }
+  & > h3 {
+    margin-top: -15px;
+    font-size: 45px;
+    font-weight: 900;
     font-family: 'Noto Serif KR', serif;
   }
 `;
