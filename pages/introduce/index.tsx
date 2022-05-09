1b5e7be98dc57c9676661f24208023f101765873
olo90632951 <olo90632951@gmail.com>
2022-05-09 16:34:03 +0900

    Add: 시퀀스 소개 페이지 추가

--- pages/introduce/index.tsx
+++ pages/introduce/index.tsx
@@ -13,42 +13,36 @@ const Introduce = () => {
         </TopBlock>
         <MainBlock>
           <SquareBlock>
-            <p className="title">
-              <b>철학</b>
-            </p>
-            <p className="content">
-              asdasdasdasdasdasasasdasdasdasdasdasdasdd
-              <br /> asdasdasdasdasdasdasdasdsaasdasdasdasdasdsad
-            </p>
+            <p className="title">문화</p>
+            <ul className="culture_content">
+              <li>1. 내일의 내가 오늘보다 나음을 지향한다</li>
+              <li>2. 서로에게 상처가 될 말들을 지양한다</li>
+              <li>3. 잘못은 인정하고 같은 실수를 반복하지 않는다</li>
+              <li>4. 다름을 존중한다</li>
+              <li>5. 완벽한 것보단 빠른 것을 추구한다</li>
+            </ul>
           </SquareBlock>
           <SquareBlock>
-            <p className="title">
-              <b>추구하는 가치</b>
-            </p>
+            <p className="title">키워드</p>
             <div className="icons">
-              <GrCloudlinux />
-              <GrStackOverflow />
-              <GrSemantics />
+              <div>
+                <GrCloudlinux />
+              </div>
+              <div>
+                <GrStackOverflow />
+              </div>
+              <div>
+                <GrSemantics />
+              </div>
             </div>
             <div className="icons_name">
-              <p>열정</p>
+              <p>근거있는 프로젝트</p>
               <p>커뮤니케이션</p>
-              <p>성장</p>
+              <p>성장지향</p>
             </div>
           </SquareBlock>
           <SquareBlock>
-            <p className="title">
-              <b>분야</b>
-            </p>
-            <p className="content">Frontend</p>
-            <p className="content">Backend</p>
-            <p className="content">DevOps</p>
-            <p className="content">Ios</p>
-          </SquareBlock>
-          <SquareBlock>
-            <p className="title">
-              <b>김지원님의 한말씀 및 간략한 소개</b>
-            </p>
+            <p className="title">김지원님의 한말씀 및 간략한 소개</p>
             <p className="content">
               ㅁㄴㅇㅁㄴㅇㅁㄴㅇ문ㅇㅁ아ㅗㅁ오만옴ㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
               <br /> asdhahskjdhaksdkaskdjhaksdhkahskdhsadhashd
--- pages/introduce/styles.tsx
+++ pages/introduce/styles.tsx
@@ -1,22 +1,9 @@
 import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
-import { sqBlack, sqWhite } from '../../styles/constants';
-
-const textAppear = keyframes`
-  0% {
-    opacity:0;
-  }
-  50% {
-    opacity:0.5;
-  }
-  100%{
-    opacity:1;
-  }
-`;
+import { sqBlack, sqLightGray, sqWhite } from '../../styles/constants';
 
 export const IntroduceBlock = styled.div`
   padding-top: 60px;
-  background-color: black;
+  background-color: ${sqWhite};
   padding-bottom: 1rem;
 `;
 
@@ -27,14 +14,26 @@ export const TopBlock = styled.div`
   text-align: center;
   font-size: 3rem;
   font-weight: bold;
-  color: ${sqWhite};
+  color: ${sqBlack};
+
+  @keyframes textAppear {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 0.5;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 
   .top1 {
-    animation: ${textAppear} 1.5s ease-in 1;
+    animation: textAppear 1.5s ease-in 1;
   }
 
   .top2 {
-    animation: ${textAppear} 1.5s ease-in 1;
+    animation: textAppear 1.5s ease-in 1;
     font-size: 1rem;
   }
 `;
@@ -44,18 +43,36 @@ export const MainBlock = styled.div`
 `;
 
 export const SquareBlock = styled.div`
-  width: 80%;
-  margin: 4rem auto;
-  height: 25rem;
-  padding: 1.5rem 3rem 1.5rem 3rem;
+  width: 60%;
+  margin: 2rem auto 6rem;
+  padding: 1.5rem 3rem 3rem 3rem;
   color: black;
-  box-shadow: 0px 12px 20px 0px rgba(240, 152, 152, 0.2);
+  box-shadow: 0px 12px 20px 0px rgba(77, 73, 73, 0.2);
   background-color: ${sqWhite};
   border-radius: 10px;
+
+  @keyframes rotateBox {
+    0% {
+      transform: rotate(-0.5deg);
+    }
+    50% {
+      transform: rotate(0.5deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+
+  &:hover {
+    animation: rotateBox 0.2s 1;
+  }
 
   .title {
     font-size: 2rem;
     text-align: center;
+    font-weight: bold;
+    font-family: Georgia, sans-serif;
+    color: brown;
   }
 
   .content {
@@ -64,24 +81,43 @@ export const SquareBlock = styled.div`
     text-align: center;
   }
 
+  .culture_content {
+    font-size: 1.3rem;
+    padding-top: 1rem;
+    text-align: center;
+    font-weight: bold;
+    list-style: none;
+    & > li {
+      padding-top: 1.5rem;
+    }
+    @media screen and (max-width: 768px) {
+      font-size: 1.3rem;
+    }
+  }
+
   .icons {
+    width: 100%;
     display: flex;
     justify-content: space-around;
     color: black;
     padding: 6rem 0 2rem 0;
-    font-size: 6rem;
+    font-size: 4.5rem;
   }
 
   .icons_name {
     display: flex;
-    justify-content: space-around;
     font-weight: bold;
-    font-size: 1.5rem;
-    width: 97.5%;
+    font-size: 1.3rem;
+    width: 100%;
     margin: 0 auto;
 
+    & > p {
+      width: 33.333%;
+      text-align: center;
+    }
+
     @media screen and (max-width: 768px) {
-      width: 92%;
+      font-size: 0.8rem;
     }
   }
 `;
