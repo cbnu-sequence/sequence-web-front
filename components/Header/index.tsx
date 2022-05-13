e3b06ad431ccb4eb2d4280774257d8535886fe53
Jiwon Kim <lost731@naver.com>
2022-05-13 23:14:25 +0900

    Add: user role 한글로 바꾸는 hook 추가

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -19,12 +19,14 @@ function Header() {
     }
   }, []);
   const [scrollPosition, setScrollPosition] = useState(0);
+
   const updateScroll = () => {
     setScrollPosition(window.scrollY || document.documentElement.scrollTop);
   };
+
   useEffect(() => {
     window.addEventListener('scroll', updateScroll);
-  });
+  }, [updateScroll]);
 
   return (
     <HeaderDiv isToggled={isToggled} userToggled={userToggled}>
