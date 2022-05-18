8b84360c145dc3b1dfb53bd83ddfa64444e82a40
Jiwon Kim <lost731@naver.com>
2022-05-18 23:26:14 +0900

    Fix: 게시글 파일과 이미지 주소 수정

--- components/PostImages/index.tsx
+++ components/PostImages/index.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { backUrl } from '../../config/config';
+
 function PostImages({ images }) {
   if (images.length === 1) {
     return (
       <>
-        <img role="presentation" src={`${backUrl}/${images[0].src}`} alt={images[0].src} />
+        <img role="presentation" src={images[0]} alt={images[0]} />
       </>
     );
   }
   if (images.length === 2) {
     return (
       <div>
-        <img role="presentation" width="50%" src={`${backUrl}/${images[0].src}`} alt={images[0].src} />
-        <img role="presentation" width="50%" src={`${backUrl}/${images[0].src}`} alt={images[1].src} />
+        <img role="presentation" width="50%" src={images[0]} alt={images[0]} />
+        <img role="presentation" width="50%" src={images[1]} alt={images[1]} />
       </div>
     );
   }
   return (
     <div>
-      <img role="presentation" width="50%" src={`http://localhost:8080/${images[0].src}`} alt={images[0].src} />
+      <img role="presentation" width="50%" src={images[0]} alt={images[0]} />
       <div
         role="presentation"
         style={{
