9061a2ed8b20e9ac93a96c0660686311942afa3f
olo90632951 <olo90632951@gmail.com>
2022-05-22 13:25:54 +0900

    Add: 프로필 프로젝트 버튼 추가

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -7,9 +7,11 @@ import Link from 'next/link';
 import { Tr } from '@chakra-ui/react';
 import CommonTd from '../../components/Table/CommonTd';
 import dayjs from 'dayjs';
+import { ProjectAddButton } from '../../components/Buttons/styles';
 
 function Profile() {
   const { user } = useUser();
+  console.log(user);
   const Router = useRouter();
   if (!user) {
     Router.replace('/');
@@ -18,20 +20,25 @@ function Profile() {
   return (
     <div>
       <Header />
-      <div>{user.name}님의 프로필</div>
+      <div>{user.data.name}님의 프로필</div>
       <h2>sequence makes difference</h2>
-      <div>이메일 주소: {user.email}</div>
-      <div>회원 등급: {user.role == 'User' ? '일반등급' : '관리자'}</div>
+      <div>이메일 주소: {user.data.email}</div>
+      <div>회원 등급: {user.data.role == 'User' ? '일반등급' : '관리자'}</div>
+      {user.data.role === 'Admin' && (
+        <Link href="/">
+          <ProjectAddButton>프로젝트 추가하기</ProjectAddButton>
+        </Link>
+      )}
       <div>
         <CommonTable headers={['번호', '작성자', '작성일', '제목']}>
-          {user.posts &&
-            user.posts.map((item, index) => {
+          {user.data.posts &&
+            user.data.posts.map((item, index) => {
               return (
                 <>
                   <Link href={`../../posts/${item._id}`}>
                     <Tr key={item._id}>
                       <CommonTd>{index + 1}.</CommonTd>
-                      <CommonTd>{user.name}</CommonTd>
+                      <CommonTd>{user.data.name}</CommonTd>
                       <CommonTd>{dayjs(item.createdAt).format('YY/MM/DD')}</CommonTd>
                       <CommonTd>{item.title}</CommonTd>
                     </Tr>
