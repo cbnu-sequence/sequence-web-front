5f616b366124a22fa0936e1e90cb69c38331af86
Jiwon Kim <lost731@naver.com>
2022-05-26 02:05:26 +0900

    Add: 이미지 캐루셀

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import React, { useRef } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../theme';
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
