078d2912f81d8b82d281342714f6e7910441b8ee
Jiwon Kim <lost731@naver.com>
2022-06-03 17:35:19 +0900

    Add: ios앱 웹뷰용 화면

--- components/NoList/index.tsx
+++ components/NoList/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { NoListDiv } from './styles';
-import Headers from '../Header';
 
 function NoList({ enTitle, krTitle }) {
   return (
     <>
-      <Headers />
       <NoListDiv>
         <div className="mainBox">
           <img className="flowerLogo_dg" src="/flowerLogo_dg.png" />
