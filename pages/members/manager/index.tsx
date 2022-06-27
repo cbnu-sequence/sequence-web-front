ecb17b0a552a59d96ee0d37efc4138173b239f58
하송희 <99heesong@naver.com>
2022-06-27 21:45:18 +0900

    운영진 프로필 한 줄 소개 추가하였습니다.

--- pages/members/manager/index.tsx
+++ pages/members/manager/index.tsx
@@ -16,8 +16,8 @@ const Manager = () => {
         <p className="title_top2">시퀀스 운영진</p>
         <div className="cardblock">
           {[
-            { _id: 'hasonghee', user: { name: '하송희' }, img: '/songhee.jpeg', description: '시퀀스 회장입니다.' },
-            { _id: 'chadonghyun', user: { name: '차동현' }, img: '/dong.jpeg', description: '시퀀스 부회장입니다.' },
+            { _id: 'hasonghee', user: { name: '하송희' }, img: '/songhee.jpeg', , description: '안녕하세요〰 시퀀스 회장과 웹 프론트 개발을 맡고있는 하송희입니다❣'},
+            { _id: 'chadonghyun', user: { name: '차동현' }, img: '/dong.jpeg', description: '안녕하세요. 시퀀스 부회장과 백엔드 개발자를 담당하는 차동현입니다.' },
           ].map((item) => (
             <ManagerCard key={item._id} className="card" onClick={() => onClickCard(item)}>
               <div>
@@ -29,9 +29,9 @@ const Manager = () => {
         </div>
         <div className="cardblock">
           {[
-            { _id: 'leechanhyuk', user: { name: '이찬혁' }, img: '/chan.jpeg', description: '시퀀스 멋쟁이입니다.' },
-            { _id: 'yoonsohee', user: { name: '윤소희' }, img: '/sohee.jpeg' },
-            { _id: 'yangkyungjin', user: { name: '양경진' }, img: '/koreaygj.jpeg' },
+            { _id: 'leechanhyuk', user: { name: '이찬혁' }, img: '/chan.jpeg', description: '안녕하세요. 시퀀스 백엔드 담당하는 이찬혁입니다.' },
+            { _id: 'yoonsohee', user: { name: '윤소희' }, img: '/sohee.jpeg', description: '안녕하세요! 시퀀스의 앱개발을 담당하고있는 윤소희입니다🙌'},
+            { _id: 'yangkyungjin', user: { name: '양경진' }, img: '/koreaygj.jpeg', description: '안녕하세요. 충북대학교 2021년도 알고리즘 대회 1등 양경진입니다! 백준 id: koreaygj✨' },
           ].map((item) => (
             <ManagerCard key={item._id} className="card" onClick={() => onClickCard(item)}>
               <div>
@@ -43,9 +43,9 @@ const Manager = () => {
         </div>
         <div className="cardblock">
           {[
-            { _id: 'kimminkyu', user: { name: '김민규' }, img: '/mk.png' },
-            { _id: 'kimjiwon', user: { name: '김지원' }, img: '/naamu.JPG' },
-            { _id: 'songhaeun', user: { name: '송하은' }, img: '/haeun.jpeg' },
+            { _id: 'kimminkyu', user: { name: '김민규' }, img: '/mk.png' , description: '🍒안녕하세요! 시퀀스 최고 귀염둥이 김민규입니다~🍒},
+            { _id: 'kimjiwon', user: { name: '김지원' }, img: '/naamu.JPG', description: '안녕하세요👋 저는 파란 하늘을 좋아하는 웹 프론트엔드 개발잡니다!' },
+            { _id: 'songhaeun', user: { name: '송하은' }, img: '/haeun.jpeg', description: '안녕하세요🤗 저는 시퀀스 웹 프론트엔드 개발자 송하은입니다🌸'},
           ].map((item) => (
             <ManagerCard key={item._id} className="card" onClick={() => onClickCard(item)}>
               <div>
