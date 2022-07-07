029fb43c1c56c1ac038419639762693c1145965f
olo90632951 <olo90632951@gmail.com>
2022-07-07 15:27:01 +0900

    Add: ssr작업 추가

--- pages/posts/edit/[id].tsx
+++ pages/posts/edit/[id].tsx
@@ -1,7 +1,33 @@
+import { useRouter } from 'next/router';
+import { dehydrate, QueryClient, useQuery } from 'react-query';
+import { getPost } from '../../../apis/post';
 import EditBoard from '../../../components/EditBoard';
 
 const PostEdit = () => {
-  return <EditBoard />;
+  const router = useRouter();
+  // @ts-ignore
+  const { id, category }: { id: string; category: string } = router.query;
+  const { isLoading, error, data } = useQuery([category, id]);
+
+  if (isLoading) return <div>로딩중입니다..</div>;
+  if (error) return <div>정보를 가져올 수 없습니다.</div>;
+  return <EditBoard data={data} id={id} category={category} />;
 };
 
 export default PostEdit;
+
+export const getServerSideProps = async (context) => {
+  try {
+    const queryClient = new QueryClient();
+    await queryClient.prefetchQuery([context.query.category, context.query.id], () =>
+      getPost(context.query.category, context.query.id),
+    );
+    return {
+      props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
