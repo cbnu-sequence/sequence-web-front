605ec0a6644c100adee0cb2618929b7c9753b672
olo90632951 <olo90632951@gmail.com>
2022-07-28 23:52:21 +0900

    Add: 뽀모도로 API함수 타입 선언

--- interfaces/pomodoro.ts
+++ interfaces/pomodoro.ts
@@ -1,3 +1,38 @@
+export interface firstPomodoroData {
+  data: {
+    _id: string;
+    title: string;
+    startDate: string;
+  };
+  message: string;
+  status: number;
+  success: boolean;
+}
+
+export interface secondPomodoroData {
+  success: boolean;
+  status: number;
+  message: string;
+}
+
+export interface myPomosData {
+  success: boolean;
+  status: number;
+  message: string;
+  count: number;
+  data: Array<{
+    _id?: string;
+    title?: string;
+    isFinished?: boolean;
+    writer?: string;
+    startDate?: string;
+    endDate?: string;
+    sequence?: number;
+    createdAt?: string;
+    updatedAt?: string
+  }>
+}
+
 export interface RankingData {
   _id: string;
   count: number;
