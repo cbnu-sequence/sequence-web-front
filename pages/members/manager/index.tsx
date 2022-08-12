7c6a957b7e95ac9cf7e1b7db9add90707b47da47
olo90632951 <olo90632951@gmail.com>
2022-08-13 00:07:46 +0900

    Add: 운영진 페이지 SSR작업 추가

--- pages/members/manager/index.tsx
+++ pages/members/manager/index.tsx
@@ -1,5 +1,9 @@
+import axios from 'axios';
 import React from 'react';
+import { dehydrate, QueryClient } from 'react-query';
+import { loadMyInfoAPI } from '../../../apis/user';
 import ManaGer from '../../../components/Manager';
+import { queryKeys } from '../../../react-query/constants';
 
 const Manager = () => {
   return <ManaGer />;
@@ -7,10 +11,23 @@ const Manager = () => {
 
 export default Manager;
 
-export const getStaticProps = async () => {
+export async function getServerSideProps(context) {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.common.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.common.Cookie = cookie;
+  }
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(queryKeys.user, () => loadMyInfoAPI(cookie));
+
+  let cleanInfo = JSON.parse(JSON.stringify(dehydrate(queryClient)));
+  if (cleanInfo.queries[0].state.data && typeof cleanInfo.queries[0].state.data != 'undefined') {
+    cleanInfo.queries[0].state.data = cleanInfo.queries[0].state.data.data;
+  }
   return {
     props: {
-      layout: 'default',
+      dehydratedState: cleanInfo,
     },
   };
-};
+}
